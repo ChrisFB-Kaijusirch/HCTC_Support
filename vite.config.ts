530d64e24050d14755f3eabba73972915d006035
ChrisFB-Kaijusirch <chris@holdingsctc.com>
2025-07-01 17:32:08 +1000

    Force new asset names to bypass GitHub Pages cache

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/HCTC_Support/',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+        entryFileNames: 'assets/app-[hash].js',
+        chunkFileNames: 'assets/chunk-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]'
+      }
+    }
+  }
 })
